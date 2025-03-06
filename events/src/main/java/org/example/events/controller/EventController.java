5b11c707b68c0adbedabb806e4d9cd2ca4072793
Guilherme Weissheimer <guilhermeweissheimer.w1@gmail.com>
2025-03-06 16:38:34 -0300

    feat: create controller

--- events/src/main/java/org/example/events/controller/EventController.java
+++ events/src/main/java/org/example/events/controller/EventController.java
@@ -1,14 +1,13 @@
 package org.example.events.controller;
 
-import org.example.events.dto.EventCreateDto;
+import org.example.events.dto.EventRequestDto;
 import org.example.events.dto.EventResponseDto;
 import org.example.events.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/events/v1")
@@ -18,8 +17,33 @@ public class EventController {
     private EventService eventService;
 
     @PostMapping("/create-event")
-    public ResponseEntity<EventResponseDto> create(@RequestBody EventCreateDto event) {
-        eventService.create(event);
-        return ResponseEntity.ok(new EventResponseDto());
+    public ResponseEntity<EventResponseDto> create(@RequestBody EventRequestDto event) {
+        return ResponseEntity.ok().body(eventService.create(event));
+    }
+
+    @GetMapping("/get-event/{id}")
+    public ResponseEntity<EventResponseDto> getEvent(@PathVariable String id) {
+        return ResponseEntity.ok().body(eventService.findById(id));
+    }
+
+    @GetMapping("/get-all-events")
+    public ResponseEntity<List<EventResponseDto>> getAllEvents() {
+        return ResponseEntity.ok().body(eventService.findAll());
+    }
+
+    @GetMapping("/get-all-events/sorted")
+    public ResponseEntity<List<EventResponseDto>> getAllEventsSorted() {
+        return ResponseEntity.ok().body(eventService.findAllSorted());
+    }
+
+    @PutMapping("update-event/{id}")
+    public ResponseEntity<EventResponseDto> updateEvent(@PathVariable String id, @RequestBody EventRequestDto event) {
+        return ResponseEntity.ok().body(eventService.update(event, id));
+    }
+
+    @DeleteMapping("/delete-event/{id}")
+    public ResponseEntity<Void> deleteEvent(@PathVariable String id) {
+        eventService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
