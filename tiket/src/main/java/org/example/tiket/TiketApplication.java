84e83bd0014ae5c85a6160336794e7c3b5cd1ec8
Guilherme Weissheimer <guilhermeweissheimer.w1@gmail.com>
2025-03-07 15:42:25 -0300

    feat: create event service with openFeign

--- tiket/src/main/java/org/example/tiket/TiketApplication.java
+++ tiket/src/main/java/org/example/tiket/TiketApplication.java
@@ -2,8 +2,10 @@ package org.example.tiket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class TiketApplication {
 
     public static void main(String[] args) {
---
+++ tiket/src/main/java/org/example/tiket/service/EventService.java
@@ -0,0 +1,13 @@
+package org.example.tiket.service;
+
+import org.example.tiket.entity.Event;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(url = "http://localhost:8080/api/events/v1")
+public interface EventService {
+
+    @GetMapping("/get-event/{id}")
+    Event getEvent(@PathVariable String id);
+}
