ad246a5d2fe9f32f85b4a1b8c3a36224b421d164
Guilherme Weissheimer <guilhermeweissheimer.w1@gmail.com>
2025-03-10 20:39:41 -0300

    test: test main events

--- events/src/test/java/org/example/events/EventsApplicationTests.java
+++ events/src/test/java/org/example/events/EventsApplicationTests.java
@@ -2,6 +2,7 @@ package org.example.events;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
 class EventsApplicationTests {
@@ -10,4 +11,8 @@ class EventsApplicationTests {
     void contextLoads() {
     }
 
+    @Test
+    void tesMain() {
+        assertDoesNotThrow(() -> EventsApplication.main(new String[]{}));
+    }
 }
